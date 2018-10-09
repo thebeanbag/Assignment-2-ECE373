f71eb27a7ffaf37c19f33197d8e815b87cc096aa
dirschn <dirschn@gmail.com>
2018-10-09 16:35:10 +0000

    some progress mostly in deck/shuffle()

--- deck.c
+++ deck.c
@@ -1,6 +1,25 @@
 #include "deck.h"
 
+
 int shuffle(){
 	printf("Shuffling deck...\n\n");
+	int count, i;
+	for(i=0; i<52; i++){
+		deck_instance.list[i].suit=C;
+		if(i>1 && i<11){
+		deck_instance.list[i].rank=(char) i
+		}
+		else{
+			switch(i) {
+				case 0 :
+					deck_instance.list[i]=J;
+					break;
 
+				case 1 :
+					deck_instance.list[i]=Q;
+					break;
+
+				case 11 :
+					deck_instance
+	}
 }
